fe68ae0af13934f759b52f7608f165fad0088f23
AmarRajanMishra <amar1998fzd@gmail.com>
2023-07-13 19:09:13 +0530

    Class-1 and Class - 2

---
+++ class-1-module/index.js
@@ -0,0 +1,26 @@
+// const modules = require('./modulesfile.js')
+import {sum, diff} from './modulesfile.js'
+
+
+console.log('Welcome to Node JS Class')
+// console.log(modules)
+// const result = modules.sum(20, 30)
+
+// console.log(result)
+
+// console.log(modules.diff(50, 10))
+// modules.fun1()
+
+// Node Modules
+console.log(sum(20, 10))
+console.log(diff(20, 10))
+
+//Node REPL
+//  Read
+// Eval
+// Print
+// Loop
+ 
+
+
+
---
+++ class-1-module/modulesfile.js
@@ -0,0 +1,30 @@
+
+
+// exports.sum = (a, b) =>{
+//     return a + b;
+// }
+
+// exports.diff = (a, b) => {
+//     return a - b;
+// }
+
+// exports.fun1 = () => {
+//     console.log('Hello world')
+// }
+
+
+// exports.sum = sum
+
+// exports.diff = diff
+
+
+const sum = (a, b) => {
+    return a + b;
+}
+
+const diff = (a, b) => {
+    return a - b;
+}
+
+
+export { sum, diff }
