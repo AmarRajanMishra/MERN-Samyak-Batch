29211a2873ed741f7bf475b23cf84e7acf88fc98
AmarRajanMishra <amar1998fzd@gmail.com>
2023-07-14 19:21:04 +0530

    Node js Class-2 Fs syncronous module

---
+++ class-2-fs-sync-module/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+
+
+// fs.writeFileSync("bio.txt", `Lorem ipsum dolor, sit amet consectetur 
+// adipisicing elit. Minus doloribus atque
+// ipsa eligendi mollitia labore eum quibusdam totam quae distinctio.`)
+
+
+// fs.appendFileSync("bio.txt", "Welcome to node js class")
+
+// 
+// const data = fs.readFileSync("bio.txt", "utf-8")
+// console.log(data)
+
+// fs.renameSync("bio.txt", "nodebio.txt")
+
+
+// fs.unlinkSync('nodebio.txt')
+
+
+// fs.mkdirSync('src')
+// fs.mkdirSync('src/public')
+// fs.writeFileSync('src/bio.txt', `Lorem ipsum dolor, sit amet`)
+// fs.writeFileSync('src/public/nodebio.txt', `Lorem ipsum dolor, sit amet`)
+
+// fs.unlinkSync('src/bio.txt')
+
+// fs.rmdirSync('src')
+
+
+
+
+
+{/* <Buffer 4c 6f 72 65 6d 20 69 70 73 75 6d 20 64 6f 6c 6f 72 2c 20 73 69 74 20 61 6d 65 74 20 63 6f 6e 73 65 63 74 65 74 75 72 20 0a 
+61 64 69 70 69 73 69 63 69 ... 123 more bytes> */}
